6c6ac0f7bc7282195cb25bec5c09b6cf02ca22e2
Anuruddha00 <anuruddhabandara06@gmail.com>
2021-05-10 09:50:53 -0800

    final

--- .history/src/Components/ToLearnMenu_20210510000849.js
+++ .history/src/Components/ToLearnMenu_20210510000849.js
@@ -7,6 +7,9 @@ function CareerGuidanceMenu () {
     return(
         <div className='primaryBodyOverview'>
             <a href='' className='primaryBodyOverviewTitle' dataIsActive='true'>Overview <span><ArrowForwardIcon/></span></a>
+            <a href='' className='primaryBodyOverviewPara1'>I want to get better<br/> at my current job<span><ArrowForwardIcon/></span></a>
+            {/* <a href='' className='primaryBodyOverviewPara2'>I want to know what<br/> I’m good at<span><ArrowForwardIcon/></span></a>
+            <a href='' className='primaryBodyOverviewPara3'>I want to find a job<br/> I’m good at<span><ArrowForwardIcon/></span></a> */}
         </div>
     )
 }
